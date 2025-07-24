8ab87b04fd623a694ac709a2161ef249243cf6ad
tinnt1 <tinnt1@runsystem.net>
2025-07-24 23:32:50 +0700

    feat: fix error websocket handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"chat-service/internal/api/routes"
-	"chat-service/internal/config"
 	"log"
 )
 
 func main() {
-	// Load configuration and start the WebSocket hub
-	config := config.Load()
-
-	// Start the WebSocket hub in a goroutine
-	go config.WSHub.WsRun()
-	log.Println("🚀 WebSocket hub started")
-
 	application, err := routes.NewApp()
 	if err != nil {
 		log.Fatalf("Failed to initialize application: %v", err)
